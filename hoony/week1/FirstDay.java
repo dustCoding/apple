5542058f9b7ece76d9de5faf49528065c31ae4fe
Hoony <44036052+hoony-lab@users.noreply.github.com>
2022-08-17 21:38:37 +0900

    Create FirstDay.java

---
+++ hoony/week1/FirstDay.java
@@ -0,0 +1,8 @@
+class FirstDay {
+    public static void main(String[] args) {
+        System.out.println("(벌써) 세번째 회사 다니고 있고, 백오피스 관련 업무 하고있습니다 ~");
+        System.out.println("알고리즘문제는 C++로 많이 풀었지만, 업무는 Java를 쓰고, Python은 찍먹해봤네요");
+        System.out.println("아무거나 편하게 질문해주셔도 됩니다 ~");
+        // 28살
+    }
+}
