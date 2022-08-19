9d480f33e2313518e1390165af243360068ad592
Centslabs <centslabs@gmail.com>
2022-08-19 14:36:08 +0900

    W01_2_baekjoon1550 완료
    16진수는 대문자만 허용한다 ★★★★★
    숏코드 print(int(input(), 16))

--- cents/week01/W01-2_baekjoon1550.py
+++ cents/week01/W01-2_baekjoon1550.py
@@ -24,24 +24,24 @@ for i in range(0,lnNum):
     if index>0:
         # print("index:",index)
         # print("i:",i)
-        if numArr[i]=='a' : sum += 10*(16**index);
-        elif numArr[i]=='b' : sum += 11*(16**index);
-        elif numArr[i]=='c' : sum += 12*(16**index);
-        elif numArr[i]=='d' : sum += 13*(16**index);
-        elif numArr[i]=='e' : sum += 14*(16**index);
-        elif numArr[i]=='f' : sum += 15*(16**index);
+        if numArr[i]=='a' or numArr[i]=='A' : sum += 10*(16**index);
+        elif numArr[i]=='b' or numArr[i]=='B': sum += 11*(16**index);
+        elif numArr[i]=='c' or numArr[i]=='C': sum += 12*(16**index);
+        elif numArr[i]=='d' or numArr[i]=='D': sum += 13*(16**index);
+        elif numArr[i]=='e' or numArr[i]=='E': sum += 14*(16**index);
+        elif numArr[i]=='f' or numArr[i]=='F': sum += 15*(16**index);
         else : sum +=int(numArr[i])*(16**index);
         # print(sum)
         
     if index==0:
         # print("index:",index)
         # print("i:",i)
-        if numArr[i]=='a' : sum += 10;
-        elif numArr[i]=='b' : sum += 11;
-        elif numArr[i]=='c' : sum += 12;
-        elif numArr[i]=='d' : sum += 13;
-        elif numArr[i]=='e' : sum += 14;
-        elif numArr[i]=='f' : sum += 15;
+        if numArr[i]=='a' or numArr[i]=='A' : sum += 10;
+        elif numArr[i]=='b' or numArr[i]=='B': sum += 11;
+        elif numArr[i]=='c' or numArr[i]=='C': sum += 12;
+        elif numArr[i]=='d' or numArr[i]=='D': sum += 13;
+        elif numArr[i]=='e' or numArr[i]=='E': sum += 14;
+        elif numArr[i]=='f' or numArr[i]=='F': sum += 15;
         else : sum +=int(numArr[i]);
         # print(sum)
     index=index-1;
