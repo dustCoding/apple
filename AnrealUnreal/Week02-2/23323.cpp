c6b83e8105f27c99f2b52a01edcd352d048732f8
WarpHero <106863351+WarpHero@users.noreply.github.com>
2022-08-26 21:39:44 +0900

    Week02-2
    //0826
    //시간초과 문제 해결
    //문제 더 단순하게 생각하기

--- AnrealUnreal/Week02-2/23323.cpp
+++ AnrealUnreal/Week02-2/23323.cpp
@@ -1,37 +1,13 @@
 //CowGotchi
-//시간초과
 #include <iostream>
 using namespace std;
-
-int cnt = 0;
-long long int Survive(long long int num1, long long int num2) {
-	cnt++;
-	num1 /= 2;
-	if (num1 > 1) {
-		return Survive(num1, num2);
-	}
-	else if (num1 == 1) {
-		if (num2 != 0) {
-			num1++;
-			num2--;
-			return Survive(num1, num2);
-		}
-		else {
-			return (int)(cnt + 1);
-		}
-	}
-	else if (num1 == 0) {
-		return (int)cnt;
-	}
-
-}
 
 int main() {
 
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int T, day;
+	int T;
 
 	long long int n, m;
 
@@ -39,9 +15,12 @@ int main() {
 
 	for (int i = 0; i < T; i++) {
 		cin >> n >> m;
-		day = Survive(n, m);
-		printf("%d \n", day);
-
+		int cnt = 0;
+		while (n) {
+			n /= 2;
+			cnt++;
+		}
+		cout << cnt + m << "\n";
 	}
 
 	return 0;
